1eaa9902d528e73d68cc069fa26138dbc05f82a4
canerkaseler <caner.kaseler@enqura.com>
2020-12-20 18:11:56 +0300

    Completed!

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,51 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  Random random = new Random();
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container(); //test
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              padding: const EdgeInsets.fromLTRB(18.0, 0.0, 9.0, 0.0),
+              onPressed: () {
+                setRandomNumberOnDices(true);
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              padding: const EdgeInsets.fromLTRB(9.0, 0.0, 18.0, 0.0),
+              onPressed: () {
+                setRandomNumberOnDices(false);
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void setRandomNumberOnDices(bool isLeft){
+    setState(() {
+      leftDiceNumber = random.nextInt(6) + 1;
+      rightDiceNumber = random.nextInt(6) + 1;
+
+      if (isLeft) print('Left button got pressed.');
+      else print('Right button got pressed.');
+    });
   }
 }
